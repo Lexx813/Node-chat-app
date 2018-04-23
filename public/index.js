a9e2d6fd0d0b02d9c64997296ded285b2da9dd26
Luis Quinones <alexx813@gmail.com>
2018-04-23 16:16:05 -0500

    done

--- public/index.js
+++ public/index.js
@@ -13,3 +13,23 @@ socket.on('disconnect', () => {
 socket.on('newMessage', function (message) {
     console.log('New Message', message);
 });
+
+socket.emit('createMessage', {
+    from:'Frank',
+    text:'Hi'
+}, function (){
+    console.log('Got it');
+    
+});
+
+
+jQuery('#message-form').on('submit', function(e){
+ e.preventDefault();
+
+ socket.emit('createMessage',{
+     from: 'User',
+     text:jQuery('[name=message').val()
+ }, function () {
+     
+ })
+})
